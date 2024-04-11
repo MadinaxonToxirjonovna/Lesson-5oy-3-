93860fe8e553484716de31999604494ed995de04
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-11 23:21:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// let maktab = {
+//     xona: 50,
+//     oquvchilar: 30,
+//     milliygvardiya: 4,
+//     ustoz: "Laylopa",
+//     oshxona: "ovqatlar"
+// }
+
+
+// for (const key in maktab) {
+//     console.log(key + ":" + maktab[key]);
+// }
+
+const myObject = { name: "John", age: 30, city: "New York", }
+
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
